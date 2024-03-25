be06f23226abd5a3166cd85f7b965d4a3d2f4898
weverton-nunes <weverton.silva@ax4b.com>
2024-03-25 16:20:11 -0300

    Adicionar arquivos de projeto.

---
+++ Temperatura/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Temperatura;
+
+Console.WriteLine("Hello, World!");
+
+var t1 = new Temp();
+t1.Teste();
---
+++ Temperatura/Temp.cs
@@ -0,0 +1,38 @@
+﻿using EasyAutomationFramework;
+using org.junit.experimental.theories.suppliers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatura
+{
+    public class Temp : WebPersonalizado
+    {
+
+        public Temp()
+        {
+
+            StartBrowser();
+        }
+
+
+        public void Teste()
+        {
+            Navigate("https://portal.inmet.gov.br/");
+
+            var cidade = "Leopoldina";
+
+            Click(TypeElement.Id, "search");
+            AssignValue(TypeElement.Id, "search", cidade);
+            Thread.Sleep(7000);
+            SelecionaItem(TypeElement.Id, "ui-id-1", cidade);
+
+
+        }
+
+    }
+
+
+}
